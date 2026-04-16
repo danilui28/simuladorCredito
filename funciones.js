c51e90f6a16d2194ab44fd5a92e3353d23f9571d
danilui28 <david82.macias@gmail.com>
2026-04-15 20:45:23 -0500

    calculo disponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,9 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos,egresos){
+    let Disponible
+    Disponible = ingresos - egresos
+    if (Disponible < 0) {
+        Disponible = 0
+    }
+    return Disponible
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,15 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
+    let Disponible=calcularDisponible(ingresos,egresos);
+
+    cambiarTexto("spnDisponible",Disponible)
+}
+
+cambiarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
+}
